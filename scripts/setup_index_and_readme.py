61f362ef454399936de7b2e0f4ebdca3dd7b2ce1
WoodenMaiden <yann.pomie@laposte.net>
2023-09-22 18:40:39 +0700

    feat: serve unstable package and list them
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- scripts/setup_index_and_readme.py
+++ scripts/setup_index_and_readme.py
@@ -1,17 +1,39 @@
 import glob
+import json
 from string import Template
 
 import sys
 
 args = sys.argv[1:]
+pre_release = False
+pre_release_files_json = None
 
-if (len(args) < 1):
-    print(f"usage: python {sys.argv[0]} <tag>")
+def print_help():
+    print(f"usage: python {sys.argv[0]} <tag> [-p|--pre-release <files_expresed_as_a_json_array_of_str>]")
+    print( "    -p, --pre-release: use this flag to indicate that the tag is a pre-release, and that the following json array of strings is the related files")
     exit(1)
 
-index_mappings = { "tag": args[0], "latest_packages": "", "all_packages": "" }
 
-readme_list = ""
+if len(args) < 1:
+    print_help()
+
+for a in args:
+    if a == "-p" or a == "--pre-release":
+        pre_release = True
+    elif pre_release:
+        pre_release_files_json = a
+
+if pre_release and not pre_release_files_json:
+    print_help()
+
+pre_release_files = []
+
+if pre_release :
+    pre_release_files = json.loads(pre_release_files_json) 
+
+
+index_mappings = { "tag": args[0], "latest_packages": "", "all_packages": "", "unstable_packages": "" }
+readme_mappings = { "latest_packages": "", "pre_release_packages": "" }
 
 packages = open("Packages").read().split("\n\n")
 packages.pop()
@@ -22,8 +44,16 @@ for package in packages:
 
     print(package)
 
-    index_mappings["latest_packages"] += f"<li><a href=\"{package['Filename']}\">{package['Package']}</a><br>Version: {package['Version']}<br>Description: {package['Description']}<br>Filename: <a href=\"{package['Filename']}\">{package['Filename'][2:-5]}</a></li>\n"
-    readme_list += f"- {package['Package']} - [{package['Filename'][2:-5]}](https://ppa.gama-platform.org/{package['Filename'][2:]})\n"
+    html     = f"<li><a href=\"{package['Filename']}\">{package['Package']}</a><br>Version: {package['Version']}<br>Description: {package['Description']}<br>Filename: <a href=\"{package['Filename']}\">{package['Filename'][2:-5]}</a></li>\n"
+    markdown = f"- {package['Package']} - [{package['Filename'][2:-5]}](https://ppa.gama-platform.org/{package['Filename'][2:]})\n"
+
+    if pre_release and package["Filename"] in pre_release_files:
+        index_mappings["unstable_packages"]     += html
+        readme_mappings["pre_release_packages"] += markdown
+    else:
+        index_mappings["latest_packages"]  += html
+        readme_mappings["latest_packages"] += markdown
+    
 
 files = glob.glob("*.deb.html")
 
@@ -37,6 +67,6 @@ f.write(template_index)
 
 
 template_readme   = Template(open("templates/README_template").read())
-template_readme   = template_readme.substitute(latest_packages=readme_list)
+template_readme   = template_readme.substitute(**readme_mappings)
 f = open("README.md", "w")
 f.write(template_readme)
--- scripts/setup_redirections.py
+++ scripts/setup_redirections.py
@@ -1,13 +1,33 @@
+import json
 from string import Template
 import sys
+import os
 
 args = sys.argv[1:]
 
+pre_release = False
+p_flag_index = -1
+
+print(args)
+for i, a in enumerate(args):
+    if pre_release:
+        pass
+    if a == "-p" or a == "--pre-release":
+        pre_release = True
+        p_flag_index = i
+
+if pre_release:
+    args.pop(p_flag_index)
+
+
 if (len(args) < 3):
-    print(f"usage: python {sys.argv[0]} <repository> <tag> <files_expresed_as_a_json_array_of_str>")
+    print(f"usage: python {sys.argv[0]} [-p|--pre-release] <repository> <tag> <files_expresed_as_a_json_array_of_str>")
+    print( "    -p, --pre-release: use this flag to indicate that the tag is a pre-release, and that the following json array of strings is the related files")
     exit(1)
 
-files = args[-1]
+print(args[-1])
+
+files = json.loads(args[-1])
 mappings = []
 headers_rules = ""
 
@@ -29,8 +49,14 @@ for m in mappings:
     f.write(content)
     filename = m["file"].split("/")[-1].split(".html")[0]
 
-    headers_rules += f"/{m['file']}.html\n\tContent-Disposition: attachment; filename=\"{filename}.deb\"\n\tLocation: https://github.com/${{ env.REPOSITORY }}/releases/download/${{ inputs.tag }}/{filename}\n\n"
+    headers_rules += f"/{m['file']}.html\n\tContent-Disposition: attachment; filename=\"{filename}.deb\"\n\tLocation: https://github.com/{ args[0] }/releases/download/{args[1]}/{filename}\n\n"
 
-headers = template_headers.substitute(headers_rules=headers_rules)
-f = open("__site/_headers", "w")
-f.write(headers)
+
+# if it is a prerelease we just append to an existing _headers file
+if not pre_release or not os.path.isfile("__site/_headers"):
+    headers = template_headers.substitute(headers_rules=headers_rules)
+    f = open("__site/_headers", "w")
+    f.write(headers)
+else:
+    f = open("__site/_headers", "a")
+    f.write(headers_rules)
