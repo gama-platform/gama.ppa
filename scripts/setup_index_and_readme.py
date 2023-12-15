9f4fffd3e38296cda95cd9a50bead603271919e5
WoodenMaiden <yann.pomie@laposte.net>
2023-12-15 20:20:35 +0100

    fix: cleanup
    Signed-off-by: WoodenMaiden <yann.pomie@laposte.net>

--- scripts/setup_index_and_readme.py
+++ scripts/setup_index_and_readme.py
@@ -1,6 +1,5 @@
 import glob
 import json
-from string import Template
 
 import sys
 
@@ -26,56 +25,3 @@ for a in args:
 if pre_release and not pre_release_files_json:
     print_help()
 
-pre_release_files = []
-
-if pre_release :
-    pre_release_files = json.loads(pre_release_files_json) 
-
-
-index_mappings = { "tag": args[0], "latest_packages": "", "all_packages": "", "alpha_packages": "" }
-readme_mappings = { "latest_packages": "", "pre_release_packages": "" }
-
-if pre_release:
-    readme_mappings["pre_release_packages"] = """
-## Pre-release/alpha packages ⚠️
-
-> [!IMPORTANT]
-> The following packages are alpha and thus not recommended.
-"""
-
-packages = open("Packages").read().split("\n\n")
-packages.pop()
-
-for package in packages:
-    package = package.split("\n")
-    package = { p.split(": ")[0]: p.split(": ")[1] for p in package }
-
-    print(package)
-
-    html     = f"<li><a href=\"{package['Filename']}\">{package['Package']}</a><br>Version: {package['Version']}<br>Description: {package['Description']}<br>Filename: <a href=\"{package['Filename']}\">{package['Filename'][2:-5]}</a></li>\n"
-    markdown = f"- {package['Package']} - [{package['Filename'][2:-5]}](https://ppa.gama-platform.org/{package['Filename'][2:]})\n"
-
-    if pre_release and ( package["Filename"] in pre_release_files or package["Package"].endswith("alpha")):
-        index_mappings["alpha_packages"]        += html
-        readme_mappings["pre_release_packages"] += markdown
-    else:
-        index_mappings["latest_packages"]  += html
-        readme_mappings["latest_packages"] += markdown
-        index_mappings["tag"] = package["Version"]
-    
-
-files = glob.glob("*.deb.html")
-
-for file in files:
-    index_mappings["all_packages"] += f"<div class=\"file-list-item\"><a href=\"/{file}\">{file[:-5]}</a><br></div>\n"
-
-template_index = Template(open("templates/index_template").read())
-template_index = template_index.substitute(**index_mappings)
-f = open("index.html", "w")
-f.write(template_index)
-
-
-template_readme   = Template(open("templates/README_template").read())
-template_readme   = template_readme.substitute(**readme_mappings)
-f = open("README.md", "w")
-f.write(template_readme)
